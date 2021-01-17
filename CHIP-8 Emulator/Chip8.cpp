15cea2df8bda76b1c0b64821ef4bec052698015d
PancakesWithSyrup <33337475+PancakesWithSyrup@users.noreply.github.com>
2021-01-17 12:01:04 -0500

    Implemented 17 instructions

--- CHIP-8 Emulator/Chip8.cpp
+++ CHIP-8 Emulator/Chip8.cpp
@@ -1,11 +1,11 @@
 #include "Chip8.h"
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 Chip8::Chip8() 
-	: randGen(std::chrono::system_clock::now().time_since_epoch().count()) {
-	randByte = std::uniform_int_distribution<int>(0, 255U);
-	
+	: randomGen(std::chrono::system_clock::now().time_since_epoch().count()) {
+	randomByte = std::uniform_int_distribution<int>(0, 255);
 	programCounter = ROM_START_ADDRESS;
 }
 
@@ -57,5 +57,206 @@ void Chip8::load_fonts() {
 	for (int x = 0; x < FONTSET_SIZE; x++) {
 		memory[FONTSET_START_ADDRESS + x] = fontSet[x];
 	}
+
+}
+
+// CLS - Clears the display
+void Chip8::OP_00E0() {
+	std::fill(video, video+2048, 0);
+}
+
+// RET - Return from a subtroutine
+void Chip8::OP_00EE() {
+	stackPointer--;
+	programCounter = stack[stackPointer];
+}
+
+// JP addr - Jump to the address
+void Chip8::OP_1nnn() {
+	uint16_t address = opcode & 0x0FFF;
+	programCounter = address;
+}
+
+// CALL addr - Calls the subroutine at the address
+void Chip8::OP_2nnn() {
+	stack[stackPointer] = programCounter;
+	stackPointer++;
+	uint16_t address = opcode & 0x0FFF;
+	programCounter = address;
+}
+
+// SE Vx, byte - Skips the next instruction if Vx = byte
+void Chip8::OP_3xkk() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, byte = opcode & 0x00FF;
+	if (registers[Vx] == byte) {
+		programCounter += 2;
+	}
+}
+
+// SNE Vx, byte - Skips the next instruction if Vx != byte
+void Chip8::OP_4xkk() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, byte = opcode & 0x00FF;
+	if (registers[Vx] != byte) {
+		programCounter += 2;
+	}
+}
+
+//SE Vx, Vy - Skips the next instruction if Vx = Vy
+void Chip8::OP_5xy0() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	if (registers[Vx] == registers[Vy]) {
+		programCounter += 2;
+	}
+}
+
+// LD Vx, byte - Sets Vx to byte
+void Chip8::OP_6xkk() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, byte = opcode & 0x00FF;
+	registers[Vx] = byte;
+}
+
+// ADD Vx, byte - Vx += byte
+void Chip8::OP_7xkk() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, byte = opcode & 0x00FF;
+	registers[Vx] += byte;
+}
+
+// LD Vx, Vy - Vx = Vy
+void Chip8::OP_8xy0() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	registers[Vx] = registers[Vy];
+}
+
+// OR Vx, Vy - Vx |= Vy
+void Chip8::OP_8xy1() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	registers[Vx] |= registers[Vy];
+}
+
+// AND Vx, Vy - Vx &= Vy
+void Chip8::OP_8xy2() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	registers[Vx] &= registers[Vy];
+}
+
+// XOR Vx, Vy - Vx ^= Vy
+void Chip8::OP_8xy3() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	registers[Vx] ^= registers[Vy];
+}
+
+// ADD Vx, Vy - Add Vx and Vy and if the result is bigger than 255 set the flag
+// register to 1, otherwise set to 0. Store the lowest 8 bits of the result in Vx.
+void Chip8::OP_8xy4() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	uint16_t sum = registers[Vx] + registers[Vy];
+	registers[Vx] = sum & 0xFF;
+	if (sum > 255) {
+		registers[15] = 1;
+	}
+	else {
+		registers[15] = 0;
+	}
+}
+
+// SUB Vx, Vy - Do Vx -= Vf and if Vx > Vf then set VF to 1, otherwise 0
+void Chip8::OP_8xy5() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	registers[Vx] -= registers[Vy];
+	if (registers[Vx] > registers[Vy]) {
+		registers[15] = 1;
+	}
+	else {
+		registers[15] = 0;
+	}
+}
+
+// SHR Vx - Set VF to 1 if the least-sig bit of Vx is 1, otherwise 0, then
+// divide Vx by 2
+void Chip8::OP_8xy6() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8;
+	registers[15] = registers[Vx] & 1;
+	registers[Vx] >>= 1;
+}
+
+// SUBN Vx, Vy - Set VF to 1 if Vy > Vx, otherwise set to 0, then subtract Vx
+// from Vy and store in Vx
+void Chip8::OP_8xy7() {
+	uint8_t Vx = (opcode & 0x0F00) >> 8, Vy = (opcode & 0x00F0) >> 4;
+	if (registers[Vy] > registers[Vx]) {
+		registers[15] = 1;
+	}
+	else {
+		registers[15] = 0;
+	}
+	registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+
+void Chip8::OP_8xyE() {
+
+}
+
+void Chip8::OP_9xy0() {
+
+}
+
+void Chip8::OP_Annn() {
+
+}
+
+void Chip8::OP_Bnnn() {
+
+}
+
+void Chip8::OP_Cxkk() {
+
+}
+
+void Chip8::OP_Dxyn() {
+
+}
+
+void Chip8::OP_Ex9E() {
+
+}
+
+void Chip8::OP_ExA1() {
+
+}
+
+void Chip8::OP_Fx07() {
+
+}
+
+void Chip8::OP_Fx0A() {
+
+}
+
+void Chip8::OP_Fx15() {
+
+}
+
+void Chip8::OP_Fx18() {
+
+}
+
+void Chip8::OP_1E() {
+
+}
+
+void Chip8::OP_Fx29() {
+
+}
+
+void Chip8::OP_Fx33() {
+
+}
+
+void Chip8::OP_Fx55() {
+
+}
+
+void Chip8::OP_Fx65() {
 
 }
--- CHIP-8 Emulator/Chip8.h
+++ CHIP-8 Emulator/Chip8.h
@@ -26,10 +26,44 @@ private:
 	uint32_t video[64 * 32];
 	uint8_t keys[16];
 	
-	
-	std::default_random_engine randGen;
-	std::uniform_int_distribution<int> randByte;
+	std::default_random_engine randomGen;
+	std::uniform_int_distribution<int> randomByte;
 
 	void load_rom(const char* romName);
 	void load_fonts();
+
+	void OP_00E0();
+	void OP_00EE();
+	void OP_1nnn();
+	void OP_2nnn();
+	void OP_3xkk();
+	void OP_4xkk();
+	void OP_5xy0();
+	void OP_6xkk();
+	void OP_7xkk();
+	void OP_8xy0();
+	void OP_8xy1();
+	void OP_8xy2();
+	void OP_8xy3();
+	void OP_8xy4();
+	void OP_8xy5();
+	void OP_8xy6();
+	void OP_8xy7();
+	void OP_8xyE();
+	void OP_9xy0();
+	void OP_Annn();
+	void OP_Bnnn();
+	void OP_Cxkk();
+	void OP_Dxyn();
+	void OP_Ex9E();
+	void OP_ExA1();
+	void OP_Fx07();
+	void OP_Fx0A();
+	void OP_Fx15();
+	void OP_Fx18();
+	void OP_1E();
+	void OP_Fx29();
+	void OP_Fx33();
+	void OP_Fx55();
+	void OP_Fx65();
 };
